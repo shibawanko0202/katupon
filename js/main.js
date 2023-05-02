759077c4e0831547e84ebe035879a578d7636cf3
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2023-05-03 00:29:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,23 +6,27 @@ const BGM = document.getElementById('bgm');
 
 BGM.volume = 0.3;
 
-document.addEventListener('click', audioPlay);
-function audioPlay(){
-  if(BGM.paused){
-    BGM.play();
-  } else {
-    BGM.pause();
-  };
-};
+// document.addEventListener('click', audioPlay);
+// function audioPlay(){
+//   if(BGM.paused){
+//     BGM.play();
+//   } else {
+//     BGM.pause();
+//   };
+// };
 
 
 // SE設定
 
-const SE = new Audio("bgm/選択音.wav");
-SE.volume = 0.1;
+const choice_SE = new Audio("bgm/選択音.wav");
+choice_SE.volume = 0.1;
+const attack_SE = new Audio("bgm/攻撃音.mp3");
+attack_SE.volume = 0.4;
 
 
 // コマンド設定
+
+const katsuya = document.getElementById("katsuya");
 
 const com_lists = document.getElementsByClassName(" command-list");
 let current_command = 0;
@@ -34,56 +38,88 @@ let check = false;
 
 
 document.addEventListener('keydown',(e)=>{
+
+  if(e.code == 'Space'){
+    if(BGM.paused){
+      BGM.play();
+    } else {
+      BGM.pause();
+    };
+  };
+
   if(!check){
     for(let i = 0;i < com_lists.length;i++){
       com_lists[i].classList.remove("blink");
     };
 
     if(e.code == 'ArrowUp'){
+
       current_command--;
       if(current_command < 0){
         current_command = 3;
       };
       com_lists[current_command].classList.add("blink");
+
     } else if(e.code == 'ArrowDown'){
+
       current_command++;
       if(current_command > 3){
         current_command = 0;
       };
       com_lists[current_command].classList.add("blink");
+
     } else if(e.code == 'Enter'){
+
       check = true;
       check_box.classList.add("apear");
       check_lists[current_check].classList.add("blink");
-
-      SE.currentTime = 0;
-      SE.play();
+      
+      choice_SE.currentTime = 0;
+      choice_SE.play();
+    } else {
+      com_lists[current_command].classList.add("blink");
     };
     
-  } else {
+  } else if(check){
+
     for(let i = 0;i < check_lists.length;i++){
       check_lists[i].classList.remove("blink");
     };
 
     if(e.code == 'ArrowUp'){
+
       current_check--;
       if(current_check < 0){
         current_check = 1;
       };
       check_lists[current_check].classList.add("blink");
+
     } else if(e.code == 'ArrowDown'){
+
       current_check++;
       if(current_check > 1){
         current_check = 0;
       };
       check_lists[current_check].classList.add("blink");
+
     } else if(e.code == 'Enter'){
+
+      if(current_check === 0){
+        attack_SE.currentTime = 0;
+        attack_SE.play();
+        katsuya.classList.add("flash");
+        setTimeout(() => {
+          katsuya.classList.remove("flash");
+        }, "1000");
+      };
+
       check = false;
       current_check = 0;
       check_box.classList.remove("apear");
       com_lists[current_command].classList.add("blink");
-    };
-    
+    } else {
+      check_lists[current_check].classList.add("blink");
+    }
   };
 });
 
