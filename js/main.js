b7b0f80c6ccc1be6f41a6f4708ab1df1bd962480
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2023-05-10 14:20:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
 "use strict"
 
+// 集計結果(当日変更、％ではなく票数) 
+
+  const s1 = 12; //『うん』 
+  const s2 = 34; //『うなちゃづけ』
+  const s3 = 23; //謀ったなー！？
+  const s4 = 7; //あ、順番来たよ
+
+  const total = s1 + s2 + s3 + s4;
+
+// 集計結果(当日変更、％ではなく票数) 
+
+// すべての音量の一括コントロール変数
+const BGMcontrol = 1;
+
+
 // BGM設定
 
-const BGM = document.getElementById('bgm');
-BGM.volume = 0.3;
+const BGM01 = document.getElementById('bgm01');
+BGM01.volume = 0.15 * BGMcontrol;
+const BGM02 = document.getElementById('bgm02');
+BGM02.volume = 0.3 * BGMcontrol;
 
-// document.addEventListener('click', audioPlay);
-// function audioPlay(){
-//   if(BGM.paused){
-//     BGM.play();
-//   } else {
-//     BGM.pause();
-//   };
-// };
+let BGMinterval;
+function BGMfeedout(){
+  BGMinterval = setInterval(()=>{
+    let feedvol = 0.02;
+    BGM02.volume = BGM02.volume - feedvol;
+    if(BGM02.volume <= 0.02){
+      clearInterval(BGMinterval);
+      BGM02.pause();
+    };
+  },300);
+};
 
 
 // SE設定
 
-const roulette_SE = new Audio("bgm/データ表示1.mp3");
-roulette_SE.volume = 0.7;
+const roulette_SE = new Audio("bgm/電子ルーレット回転中.mp3");
+roulette_SE.volume = 0.2 * BGMcontrol;
+const bigger_SE = new Audio("bgm/警告音1.mp3");
+bigger_SE.volume = 0.6 * BGMcontrol;
 const start_SE = new Audio("bgm/戦火を交えて(戦闘開始).mp3");
-start_SE.volume = 0.3;
+start_SE.volume = 0.3 * BGMcontrol;
 const choice_SE = new Audio("bgm/選択音.wav");
-choice_SE.volume = 0.1;
+choice_SE.volume = 0.1 * BGMcontrol;
 const attack_SE = new Audio("bgm/攻撃音.mp3");
-attack_SE.volume = 0.4;
+attack_SE.volume = 0.4 * BGMcontrol;
+const serif_SE = new Audio("bgm/会話_単音.mp3");
+serif_SE.volume = 0.2 * BGMcontrol;
+const gameover_SE = new Audio("bgm/ゲームオーバー.mp3");
+gameover_SE.volume = 0.3 * BGMcontrol;
 
 
 // グラフ画面
@@ -32,18 +58,26 @@ attack_SE.volume = 0.4;
 const graph = document.getElementById("graph");
 const lines = document.getElementsByClassName("line");
 const values = document.getElementsByClassName("value");
+const boxs = document.getElementsByClassName("box");
 
 let graph_switch = true;
-let rotate_switch = false;
+let rotate_switch = 1;
 
-let select_01 = 18;
-let select_02 = 36;
-let select_03 = 12;
-let select_04 = 34;
-let v01 = 0;
-let v02 = 0;
-let v03 = 0;
-let v04 = 0;
+
+// ％変換
+const select_01 = Math.round(s1 / total * 100); 
+const select_02 = Math.round(s2 / total * 100);
+const select_03 = Math.round(s3 / total * 100);
+const select_04 = Math.round(s4 / total * 100);
+const select_array = [select_01,select_02,select_03,select_04];
+// 配列の最大数のインデックス
+const max_num = Math.max(...select_array);
+const max_select = select_array.indexOf(max_num);
+
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
 
 // コマンド設定
 
@@ -57,170 +91,415 @@ const check_lists = document.getElementsByClassName(" check-list");
 let current_check = 0;
 let check = false;
 
-const text = document.getElementById("text");
-
+const flashscreen = document.getElementById("flashscreen");
 const blackout = document.getElementById("blackout");
 const main = document.getElementById("main");
+const bgi = document.getElementById("bgi");
+const gameovers = document.getElementsByClassName("gameover");
+const frames = document.getElementsByClassName("frame");
+const textbox = document.getElementById("textbox");
+let control = true;
+
+// グラフアニメーション用の変数
+const anim_sec = 50;
+
+// テキストの内容
+const texts = document.getElementsByClassName("text");
+let text_interval;
+const text00 = ["カツヤが　あらわれた！","カツヤは　『けっこんしよう』　と言っている…。"];
+const text01 = ["カツヤ「・・・。"];
+const text02 = [
+  "カツヤ「いや、そうじゃなくて・・・。",
+  "いずほ「え？『け』でしょ？",
+  "カツヤ「・・・・・・・・・・・・",
+  "　　　　・・・・・・『けいえいはたん』"
+];
+const text03 = ["カツヤ「・・・。"];
+const text04 = ["カツヤ「あ、そっすね・・・。","いずほ「はやく行こっ！"];
+
+// コントロールON/OFF
+
+  function control_on(){
+    control = true;
+    com_lists[current_command].classList.add("blink");
+  };
+
+  function control_off(){
+    control = false;
+  };
+
+// コントロールON/OFF
+
+// テキスト表示アニメーション関数
+
+  function word(t){
+    for(let i = 0;i < texts.length;i++){
+      texts[i].innerHTML = null;
+    };
+
+    let len_total = 0;
+
+    for(let i = 0;i < t.length;i++){
+      let n = 0;
+      let len = t[i].length;
+      if(i >= 1){
+        len_total += len;
+      };
+
+      setTimeout(()=>{
+        text_interval = setInterval(()=>{
+          serif_SE.currentTime = 0;
+          serif_SE.play();
+          texts[i].innerHTML = t[i].slice(0,n);
+          //HTMLのtextに、変数sの０文字目からn文字までのテキストを表示する
+          if(n < len){
+            //文字を増やす処理の回数が入力された文字数を超えるまで繰り返す
+            n++;
+          } else {
+            //文字を増やす処理の回数が入力された文字数を超えた時の処理
+            clearInterval(text_interval);
+          };
+        },50);
+      },(len_total * 100));
+    };
+  };
+
+// テキスト表示アニメーション関数
+
+// ゲームオーバー関数
+
+  function gameover_anime(i){
+    setTimeout(()=>{
+      gameover_SE.currentTime = 0;
+      gameover_SE.play();
+      BGM02.pause();
+      for(let i = 0;i < gameovers.length;i++){
+        gameovers[i].classList.add("over");
+      };
+      for(let i = 0;i < com_lists.length;i++){
+        com_lists[i].classList.remove("blink");
+      };
+    },(i * 1000));
+  };
+
+// ゲームオーバー関数
+
+// バトル開始関数
+
+  function battle(){
+
+    new Promise((resolve)=>{
+      control_off();
+      com_lists[current_command].classList.remove("blink");
+      start_SE.currentTime = 0;
+      start_SE.play();
+      setTimeout(()=>{
+        resolve();
+      },5600);
+    }).then(()=>{
+      // 戦火を交えて(ループ)開始
+      BGM02.play();
+    });
+
+    new Promise((resolve) =>{
+      
+      flashscreen.classList.add("in");
+      setTimeout(()=>{
+        resolve();
+      },1200);
+
+    }).then(()=>{
+              
+      new Promise((resolve)=>{
+
+        // 背景導入
+        bgi.classList.add("apear");
+        textbox.classList.add("apear");
+        katsuya.classList.add("in");
+        setTimeout(()=>{
+          bgi.classList.add("apear2");
+        },2000);
+        setTimeout(()=>{
+          resolve();
+        },2500);
+
+      }).then(()=>{
+
+        new Promise((resolve)=>{
+          
+          // カツヤ登場
+          setTimeout(()=>{
+            word(text00);
+          },1800);
+          setTimeout(()=>{
+            resolve();
+          },3000);
+
+        }).then(()=>{
+          
+          for(let i = 0;i < frames.length;i++){
+            setTimeout(()=>{
+              frames[i].classList.add("apear");
+            },(i * 60))
+          };
+
+          setTimeout(()=>{
+            control_on();
+          },4000);
+
+        });
+      });
+    });
+  };
+
+// バトル開始関数
+
+// 確認用
+
+// graph_switch = false;
+// graph.classList.add("off");
+// main.classList.remove("blur");
+// control_off();
+
+// document.addEventListener('keydown',(e)=>{
+//   battle();
+// })
+
+// 確認用
 
 
+// キーを打鍵してコントロール
 document.addEventListener('keydown',(e)=>{
 
   if(graph_switch){
+    // グラフ画面
+
+    if(e.code == 'Space'){
+      if(BGM01.paused){
+        BGM01.play();
+      } else {
+        BGM01.pause();
+      };
+    };
 
     if(e.code == 'Enter'){
-      if(!rotate_switch){
+      if(rotate_switch == 1){
+        rotate_switch++;
+
         for(let i = 0;i < lines.length;i++){
           lines[i].classList.add("rotate");
         };
-        rotate_switch = true;
+        const rotates = document.getElementsByClassName("rotate");
+
+        // 円グラフアニメーション追加
+        rotates[0].style.strokeDashoffset = `calc(440 - (440 * ${select_01}) / 100)`;
+        rotates[0].style.animation = `circleAnim ${(select_01 * anim_sec) / 1000}s forwards`;
+        
+        rotates[1].style.strokeDashoffset = `calc(440 - (440 * ${select_02}) / 100)`;
+        rotates[1].style.animation = `circleAnim ${(select_02 * anim_sec) / 1000}s forwards`;
+        
+        rotates[2].style.strokeDashoffset = `calc(440 - (440 * ${select_03}) / 100)`;
+        rotates[2].style.animation = `circleAnim ${(select_03 * anim_sec) / 1000}s forwards`;
+        
+        rotates[3].style.strokeDashoffset = `calc(440 - (440 * ${select_04}) / 100)`;
+        rotates[3].style.animation = `circleAnim ${(select_04 * anim_sec) / 1000}s forwards`;
 
         roulette_SE.currentTime = 0;
         roulette_SE.play();
+        setTimeout(()=>{
+          roulette_SE.pause();
+        },(max_num * anim_sec));
         
-        // for(let i = 0;i < values.length;i++){
-        // };
+        // カウントUPアニメーション
         const countup01 = setInterval(()=>{
-          v01++;
-          values[0].textContent = v01;
-          if(v01 == select_01){
+          count1++;
+          values[0].textContent = count1;
+          if(count1 == select_01){
             clearInterval(countup01);
           };
-        },24);
+        },anim_sec);
         const countup02 = setInterval(()=>{
-          v02++;
-          values[1].textContent = v02;
-          if(v02 == select_02){
+          count2++;
+          values[1].textContent = count2;
+          if(count2 == select_02){
             clearInterval(countup02);
           };
-        },24);
+        },anim_sec);
         const countup03 = setInterval(()=>{
-          v03++;
-          values[2].textContent = v03;
-          if(v03 == select_03){
+          count3++;
+          values[2].textContent = count3;
+          if(count3 == select_03){
             clearInterval(countup03);
           };
-        },24);
+        },anim_sec);
         const countup04 = setInterval(()=>{
-          v04++;
-          values[3].textContent = v04;
-          if(v04 == select_04){
+          count4++;
+          values[3].textContent = count4;
+          if(count4 == select_04){
             clearInterval(countup04);
           };
-        },24);
+        },anim_sec);
         
-      }else{
-        graph.classList.add("off");
-        main.classList.remove("blur");
-        graph_switch = false;
+      } else if(rotate_switch == 2){
 
-        start_SE.currentTime = 0;
-        start_SE.play();
+        rotate_switch++;
         setTimeout(()=>{
-          BGM.play();
-        },5600);
-        setTimeout(()=>{
-          com_lists[current_command].classList.add("blink");
-        },5950);
+          bigger_SE.currentTime = 0;
+          bigger_SE.play();
+        },800);
+        for(let i = 0;i < boxs.length;i++){
+          boxs[i].classList.add("small");
+        };
+        boxs[max_select].classList.remove("small");
+        boxs[max_select].classList.add("big");
+
+      } else if(rotate_switch == 3){
+
+        graph.classList.add("blur");
+        main.classList.remove("off");
+        BGM01.pause();
+        graph_switch = false;
+        battle();
+
       };
     };
 
   } else {
+    // グラフ画面終了後
+
+    if(control){
+
+      if(e.code == 'Space'){
+        if(BGM02.paused){
+          BGM02.play();
+        } else {
+          BGM02.pause();
+        };
+      };
     
-    if(e.code == 'Space'){
-      if(BGM.paused){
-        BGM.play();
-      } else {
-        BGM.pause();
-      };
-    };
-  
-    if(!check){
-      // 確認画面
-      for(let i = 0;i < com_lists.length;i++){
-        com_lists[i].classList.remove("blink");
-      };
-  
-      if(e.code == 'ArrowUp'){
-  
-        current_command--;
-        if(current_command < 0){
-          current_command = 3;
+      if(!check){ //4択画面
+
+        for(let i = 0;i < com_lists.length;i++){
+          com_lists[i].classList.remove("blink");
         };
-        com_lists[current_command].classList.add("blink");
-  
-      } else if(e.code == 'ArrowDown'){
-  
-        current_command++;
-        if(current_command > 3){
-          current_command = 0;
-        };
-        com_lists[current_command].classList.add("blink");
-  
-      } else if(e.code == 'Enter'){
-  
-        check = true;
-        check_box.classList.add("apear");
-        check_lists[current_check].classList.add("blink");
         
-        choice_SE.currentTime = 0;
-        choice_SE.play();
-      } else {
-        com_lists[current_command].classList.add("blink");
-      };
-      
-    } else if(check){
-      // 4択画面
-      for(let i = 0;i < check_lists.length;i++){
-        check_lists[i].classList.remove("blink");
-      };
-  
-      if(e.code == 'ArrowUp'){
-  
-        current_check--;
-        if(current_check < 0){
-          current_check = 1;
-        };
-        check_lists[current_check].classList.add("blink");
-  
-      } else if(e.code == 'ArrowDown'){
-  
-        current_check++;
-        if(current_check > 1){
-          current_check = 0;
-        };
-        check_lists[current_check].classList.add("blink");
-  
-      } else if(e.code == 'Enter'){
-  
-        if(current_check === 0){
-          attack_SE.currentTime = 0;
-          attack_SE.play();
-          katsuya.classList.add("flash");
-          setTimeout(() => {
-            katsuya.classList.remove("flash");
-          }, "1000");
-  
-          if(current_command == 0){
-            text.textContent = "・・・";
-            blackout.classList.add("in");
-          }else if(current_command == 1){
-            text.textContent = "け、『けいえいはたん』";
-          }else if(current_command == 2){
-            text.textContent = "・・・";
-          }else if(current_command == 3){
-            text.textContent = "あ、そっすね・・・";
+        if(e.code == 'ArrowUp'){
+          //4択画面で「↑」を押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+          
+          current_command--;
+          if(current_command < 0){
+            current_command = 3;
           };
+          com_lists[current_command].classList.add("blink");
+          
+        } else if(e.code == 'ArrowDown'){
+          //4択画面で「↓」を押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+          
+          current_command++;
+          if(current_command > 3){
+            current_command = 0;
+          };
+          com_lists[current_command].classList.add("blink");
+          
+        } else if(e.code == 'Enter'){
+          //4択画面でエンターを押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+    
+          check = true;
+          check_box.classList.add("open");
+          check_lists[current_check].classList.add("blink");
+          
+        } else {
+          com_lists[current_command].classList.add("blink");
         };
+        
+      } else if(check){ //確認画面
   
-        check = false;
-        current_check = 0;
-        check_box.classList.remove("apear");
-        com_lists[current_command].classList.add("blink");
-      } else {
-        check_lists[current_check].classList.add("blink");
-      }
+        for(let i = 0;i < check_lists.length;i++){
+          check_lists[i].classList.remove("blink");
+        };
+        
+        if(e.code == 'ArrowUp'){
+          //確認画面で「↑」を押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+          
+          current_check--;
+          if(current_check < 0){
+            current_check = 1;
+          };
+          check_lists[current_check].classList.add("blink");
+          
+        } else if(e.code == 'ArrowDown'){
+          //確認画面で「↓」を押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+          
+          current_check++;
+          if(current_check > 1){
+            current_check = 0;
+          };
+          check_lists[current_check].classList.add("blink");
+          
+        } else if(e.code == 'Enter'){
+          //確認画面でエンターを押したとき
+
+          choice_SE.currentTime = 0;
+          choice_SE.play();
+          
+          if(current_check === 0){
+            // 「はい」の場合
+            attack_SE.currentTime = 0;
+            attack_SE.play();
+            katsuya.classList.add("flash");
+            setTimeout(() => {
+              katsuya.classList.remove("flash");
+            }, "1000");
+            control_off();
+    
+            if(current_command == 0){
+              setTimeout(()=>{
+                word(text01);
+                blackout.classList.add("in");
+                BGMfeedout();
+              },1000);
+            }else if(current_command == 1){
+              setTimeout(()=>{
+                word(text02);
+              },1000);
+              gameover_anime(10);
+            }else if(current_command == 2){
+              setTimeout(()=>{
+                word(text03);
+              },1000);
+            }else if(current_command == 3){
+              setTimeout(()=>{
+                word(text04);
+              },1000);
+              gameover_anime(6);
+            };
+          }; //　「はい」の場合
+    
+          check = false;
+          current_check = 0;
+          check_box.classList.remove("open");
+          com_lists[current_command].classList.add("blink");
+        } else { //確認画面でその他のキーを押したとき
+          // 最初に"blink"を全部消してるので必要なのだ
+          check_lists[current_check].classList.add("blink");
+        };
+      };
     };
   };
-
 });
 
